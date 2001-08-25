5fd07c971dce3adcfbfb737111d9988d1672b02f
greghmerrill <greghmerrill@2de6f20a-cb13-0410-bab0-9f1e610651cd>
2001-08-25 16:31:46 +0000

    copyright update
    git-svn-id: https://follow.svn.sourceforge.net/svnroot/follow/trunk@35 2de6f20a-cb13-0410-bab0-9f1e610651cd

--- follow/src/ghm/followgui/DndFileOpener.java
+++ follow/src/ghm/followgui/DndFileOpener.java
@@ -1,8 +1,21 @@
-// Copyright (C) 2000 Greg Merrill (greghmerrill@yahoo.com)
-// Distributed under the terms of the GNU General Public License (version 2)
-// For details on the GNU GPL, please visit http://www.gnu.org/copyleft/gpl.html
-// To find out more about this and other free software by Greg Merrill, 
-//  please visit http://gregmerrill.imagineis.com
+/* 
+Copyright (C) 2000, 2001 Greg Merrill (greghmerrill@yahoo.com)
+
+This file is part of Follow (http://follow.sf.net).
+
+Follow is free software; you can redistribute it and/or modify
+it under the terms of version 2 of the GNU General Public
+License as published by the Free Software Foundation.
+
+Follow is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Follow; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
 
 package ghm.followgui;
 

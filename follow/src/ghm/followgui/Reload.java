a80584f56cd6e9c937f24e6da2e06a2a226afe46
murali_ca_us <murali_ca_us@2de6f20a-cb13-0410-bab0-9f1e610651cd>
2003-06-21 04:15:18 +0000

    Removed an unused import.
    git-svn-id: https://follow.svn.sourceforge.net/svnroot/follow/trunk@53 2de6f20a-cb13-0410-bab0-9f1e610651cd

--- follow/src/ghm/followgui/Reload.java
+++ follow/src/ghm/followgui/Reload.java
@@ -20,7 +20,6 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 package ghm.followgui;
 
 import java.awt.event.ActionEvent;
-import java.io.File;
 
 /**
 Action which closes the currently followed file.

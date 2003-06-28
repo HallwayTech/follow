86ff70d8ca5ba82391a016ecf09a38894813090f
murali_ca_us <murali_ca_us@2de6f20a-cb13-0410-bab0-9f1e610651cd>
2003-06-28 16:58:21 +0000

    Added website files to version control.
    git-svn-id: https://follow.svn.sourceforge.net/svnroot/follow/trunk@59 2de6f20a-cb13-0410-bab0-9f1e610651cd

---
+++ follow/doc/web/footer.js
@@ -0,0 +1,5 @@
+document.writeln('<p align=right><br>');
+document.writeln('<span class=footer>');
+document.writeln('page last updated ' + document.lastModified + '<br>');
+document.writeln('</span>');
+

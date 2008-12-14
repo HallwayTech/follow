fc806fddb2ab632c6af654927714dc671f16cbe7
bigdaddydsp <bigdaddydsp@2de6f20a-cb13-0410-bab0-9f1e610651cd>
2008-12-14 21:24:47 +0000

    Fixed up project to use maven for publishing the site.  Updated pics.
    git-svn-id: https://follow.svn.sourceforge.net/svnroot/follow/trunk@180 2de6f20a-cb13-0410-bab0-9f1e610651cd

--- src/site/footer.js
+++
@@ -1,4 +0,0 @@
-document.writeln('<p align=right><br>');
-document.writeln('<span class=footer>');
-document.writeln('page last updated ' + document.lastModified + '<br>');
-document.writeln('</span>');

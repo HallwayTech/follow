931dc411cf36ede8f9e1f5ceec3d07f058ca7d3f
bigdaddydsp <bigdaddydsp@2de6f20a-cb13-0410-bab0-9f1e610651cd>
2009-09-12 19:57:47 +0000

    Split gui test into private/shared tests.
    git-svn-id: https://follow.svn.sourceforge.net/svnroot/follow/branches/1.7@194 2de6f20a-cb13-0410-bab0-9f1e610651cd

---
+++ src/test/java/ghm/follow/test/gui/PrivateAppTest.java
@@ -0,0 +1,9 @@
+package ghm.follow.test.gui;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses( { CommandLineT.class })
+public class PrivateAppTest {
+}
